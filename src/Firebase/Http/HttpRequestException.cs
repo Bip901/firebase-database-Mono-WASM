03b8210285a0ccd0797c841dedbf9821d4e046ff
Bip901 <74911767+Bip901@users.noreply.github.com>
2025-01-10 18:49:56 +0200

    Use a dedicated exception type for http errors

---
+++ src/Firebase/Http/HttpRequestException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Firebase.Database.Http
+{
+    /// <summary>
+    /// An exception thrown when an HTTP response contains a non-success status code.
+    /// </summary>
+    public class HttpRequestException : Exception
+    {
+        /// <summary>
+        /// The received status code.
+        /// </summary>
+        public int StatusCode { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="HttpRequestException"/>.
+        /// </summary>
+        public HttpRequestException(int statusCode, Exception inner = null) : base($"Received non-success status code {statusCode}", inner)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
--- src/Firebase/Http/HttpResponseMessage.cs
+++ src/Firebase/Http/HttpResponseMessage.cs
@@ -21,17 +21,19 @@ namespace Firebase.Database.Http
         /// <summary>
         /// Throws an exception if <see cref="StatusCode"/> was not in the range 200-299.
         /// </summary>
-        /// <returns></returns>
+        /// <exception cref="HttpRequestException"/>
+        /// <returns>this.</returns>
         public HttpResponseMessage EnsureSuccessStatusCode()
         {
             int statusCodeInt = (int)StatusCode;
             if (statusCodeInt < 200 || statusCodeInt > 299)
             {
-                throw new Exception($"Bad status code {statusCodeInt}");
+                throw new HttpRequestException(statusCodeInt);
             }
             return this;
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             Content.Dispose();
